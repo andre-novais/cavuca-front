893e833d64ecc5e145acea4f2763e90742ca28e0
André Novais <andrenovais.git@gmail.com>
2021-06-19 16:14:19 -0300

    adding google analytics to site

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
+import VueGtag from "vue-gtag-next"
 
 createApp(App)
   .use(router)
+  .use(VueGtag, {
+    property: {
+      id: process.env.VUE_APP_GOOGLE_ANALYTICS_ID,
+      router
+    }
+  })
   .mount("#app")
